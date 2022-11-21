4e1e866d60855ed6ba992abbb2282e75319e88a7
Denis <ivdenvas@gmail.com>
2022-11-21 17:34:41 +0300

    added example text

---
+++ Example_for/Program.cs
@@ -0,0 +1,27 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012
+// s[3]  // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string resault = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) resault = resault + $"{newValue}";
+        else resault = resault + $"{text[i]}";
+    }
+    return resault;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
