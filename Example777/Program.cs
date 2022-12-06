35615588d2a5f4b507e89768a5386aad49d638bd
Denis <ivdenvas@gmail.com>
2022-12-06 17:10:54 +0300

    fe

---
+++ Example777/Program.cs
@@ -0,0 +1,40 @@
+﻿//
+
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//     if (length == word.Length)
+//     {
+//         Console.WriteLine($"{n++} {new String(word)}"); return;
+//     }
+//     for (int i = 0; i < alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWords(alphabet, word, length + 1);
+//     }
+// }
+
+// FindWords("аисв", new char[2]);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//алгоритм обхожа директории
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + "  ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = @"/Users/User/Desktop/GB/Examples/Example001_HelloConsole";
+CatalogInfo(path);
